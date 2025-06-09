a534836b7dabb09a741a3180c0b985dad9dee464
Victor Maznichenko <victor_maznichenko@mail.ru>
2025-06-09 15:46:43 +0300

    fix: fixed tox

--- media_processor/__init__.py
+++ media_processor/__init__.py
@@ -3,15 +3,15 @@ Media Processor Package
 This package provides utilities for processing media files.
 """
 
-from audio_video_utils import (
+from .audio_video_utils import (
     convert_audio,
     adjust_volume,
     normalize_audio,
     merge_audio_files,
 )
-from image_utils import apply_blur_filter
-from graphics_utils import add_watermark
-from pdf_utils import merge_pdfs
+from .image_utils import apply_blur_filter
+from .graphics_utils import add_watermark
+from .pdf_utils import merge_pdfs
 
 __all__ = [
     "convert_audio",
